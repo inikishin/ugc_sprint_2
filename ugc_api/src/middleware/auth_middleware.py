82301f1784793a0c230fb597aa8c898cf46b6eeb
darkstorn <inikishin@gmail.com>
2022-12-26 17:29:22 +0300

    add http status

--- ugc_api/src/middleware/auth_middleware.py
+++ ugc_api/src/middleware/auth_middleware.py
@@ -1,5 +1,6 @@
 import aiohttp
 
+from http import HTTPStatus
 from fastapi import Request, FastAPI
 from fastapi.responses import Response
 from starlette.middleware.base import BaseHTTPMiddleware
@@ -14,15 +15,19 @@ class AuthMiddleware(BaseHTTPMiddleware):
         auth_header = request.headers.get('Authorization')
 
         if auth_header is None:
-            return Response('Authorization header is missing', 401)
+            return Response('Authorization header is missing',
+                            HTTPStatus.UNAUTHORIZED)
 
         async with aiohttp.ClientSession() as session:
             auth = await session.get(self.auth_url,
-                                     headers={'Authorization': auth_header},
+                                     headers={
+                                         'Authorization': auth_header,
+                                     },
                                      )
 
-            if auth.status == 401:
-                return Response('Not authorized', 401)
+            if auth.status == HTTPStatus.UNAUTHORIZED:
+                return Response('Not authorized',
+                                HTTPStatus.UNAUTHORIZED)
 
         response = await call_next(request)
 
